59a74d64051052c830bb8c4afab8748333f352eb
JonathanZeru <jonathanzeru21@gmail.com>
2024-08-29 08:33:14 +0300

    voice recording added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,17 @@
 import UIKit
 import Flutter
+import Firebase // Add this import statement
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Configure Firebase
+    FirebaseApp.configure() // Add this line to configure Firebase
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
